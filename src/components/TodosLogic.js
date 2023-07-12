80ff8b7454510c5fbed105f882b1a6e0743624e7
rbd3 <rabedesanaa@gmail.com>
2023-07-12 10:37:37 +0300

    Fix linters error

--- src/components/TodosLogic.js
+++ src/components/TodosLogic.js
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import InputTodo from './InputTodo';
-import TodosList
- from './TodoList';
+import TodosList from './TodoList';
 
 const TodosLogic = () => {
   const [todos, setTodos] = useState([
